d5c4d1437a9884c14b31eba26d0e713340511c6e
Kunsh Sabharwal <sabharwalkunsh05@gmail.com>
2026-02-07 19:01:44 +0530

    Added exp 1.1 code and image

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -22,16 +22,22 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 102, 191, 95),
+      backgroundColor: const Color.fromARGB(255, 0, 132, 9),
       body: Center(
-        child: Text(
-          'Hello, Kunsh Sabharwal!',
-          style: TextStyle(
-            color: const Color.fromARGB(255, 0, 48, 6),
-            fontSize: 32.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Times New Roman',
-          ),
+        child: Column(
+          children: [
+            Image.asset('assets/image.jpg', height: 400, width: 400),
+            SizedBox(height: 200),
+            Text(
+              'Random Nature Image',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 0, 48, 6),
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Times New Roman',
+              ),
+            ),
+          ],
         ),
       ),
     );
